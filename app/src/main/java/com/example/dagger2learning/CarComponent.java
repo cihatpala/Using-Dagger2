e2ccfd3623ed7e9c39335e9fd1a3f37960a1befb
Cihat PALA <cihat.pala@dgpays.com>
2021-01-28 12:51:50 +0300

    Dagger CarComponent

---
+++ app/src/main/java/com/example/dagger2learning/CarComponent.java
@@ -0,0 +1,8 @@
+package com.example.dagger2learning;
+
+import dagger.Component;
+
+@Component
+public interface CarComponent {
+    Car getCar();
+}
---
+++ app/src/main/java/com/example/dagger2learning/Wheels.java
@@ -0,0 +1,11 @@
+package com.example.dagger2learning;
+
+import javax.inject.Inject;
+
+public class Wheels {
+
+    @Inject
+    public Wheels(){
+
+    }
+}
